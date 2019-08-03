d57fb1886d3b2f38612433002e7412054f8c8b90
zerdzhong <zhongzhendongchn@gmail.com>
2019-08-03 02:25:46 +0000

    update travis install cmake shell

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -5,13 +5,15 @@ if [ "$TRAVIS_OS_NAME" != "linux" ]; then
     exit 0;
 fi
 
-DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+if ! [ -x "$(command -v cmake)" ]; then
+    DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
 
-mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
+    mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
 
-wget --no-check-certificate https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.tar.gz
-tar -xvf cmake-3.12.3-Linux-x86_64.tar.gz > /dev/null
-mv cmake-3.12.3-Linux-x86_64 cmake-install
-PATH=${DEPS_DIR}/cmake-install:${DEPS_DIR}/cmake-install/bin:$PATH
-echo $PATH
-cd ${TRAVIS_BUILD_DIR}
+    wget --no-check-certificate https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.tar.gz
+    tar -xvf cmake-3.12.3-Linux-x86_64.tar.gz > /dev/null
+    mv cmake-3.12.3-Linux-x86_64 cmake-install
+    PATH=${DEPS_DIR}/cmake-install:${DEPS_DIR}/cmake-install/bin:$PATH
+    echo $PATH
+    cd ${TRAVIS_BUILD_DIR}
+fi
