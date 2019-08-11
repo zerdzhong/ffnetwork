f56a05ae936b5f6e20c2d800b4ecdc4fd714bf87
zerdzhong <zhongzhendongchn@gmail.com>
2019-08-11 09:32:15 +0000

    config code coverage

--- ci/travis.sh
+++ ci/travis.sh
@@ -5,8 +5,9 @@ set -e
 cmake --version
 mkdir build || true
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=1
 make
 CTEST_OUTPUT_ON_FAILURE=1 make unittest
 cd unittest
 ctest --verbose
+make ffnetwork_coverage
