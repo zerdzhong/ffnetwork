2d251809abf79f36db29c2aff90d85c72628ef6b
zerdzhong <zhongzhendongchn@gmail.com>
2019-08-11 13:35:17 +0000

    ignore erro while make coverage

--- ci/travis.sh
+++ ci/travis.sh
@@ -6,12 +6,12 @@ cmake --version
 mkdir build || true
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=1
-make
+make -j 6
 CTEST_OUTPUT_ON_FAILURE=1 make unittest
 cd unittest
 ctest --verbose
 
 set +e
-//disable exit on non 0
-make clean
-make ffnetwork_coverage
+#disable exit on non 0
+make ffnetwork_coverage || true
+echo "finish"
