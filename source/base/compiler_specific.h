b36c6f0748565c79dbc5134486cf0ad4a24ef1e3
zerdzhong <zhongzhendongchn@gmail.com>
2019-10-03 07:35:11 +0800

    fix misspell

--- source/base/compiler_specific.h
+++ source/base/compiler_specific.h
@@ -13,19 +13,19 @@
     #define FF_ALLOW_UNUSED_TYPE
 #endif
 
-#if defined(__GUNC__) || defined(__clang__)
+#if defined(__GNUC__) || defined(__clang__)
     #define FF_NOINLINE __attribute__((noinline))
 #elif defined(_MSC_VER)
     #define FF_NOINLINE __declspec(noinline)
 #endif
 
-#if defined(__GUNC__) || defined(__clang__)
+#if defined(__GNUC__) || defined(__clang__)
     #define FF_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
 #elif defined(_MSC_VER)
     #define FF_WARN_UNUSED_RESULT
 #endif
 
-#if defined(__GUNC__) || defined(__clang__)
+#if defined(__GNUC__) || defined(__clang__)
     #define FF_PRINTF_FORMAT(format_param, dots_param)  \
         __attribute__((format(printf, format_param, dots_param)))
 #else
